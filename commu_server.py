0c49e3c3e35947494a685870d9e35bb205ac47e1
Alfred <gdzzc96@gmail.com>
2021-06-18 23:09:54 +0800

    modify server

--- commu_server.py
+++ commu_server.py
@@ -31,40 +31,8 @@ def disconnect(sid):
     print('disconnect ', sid)
 
 
-def background_thread():
-    count = 0
-
-    while True:
-        socketio.sleep(5)
-
-        count += 1
-
-        t = time.strftime('%M:%S', time.localtime())
-
-        # 获取系统时间（只取分:秒）
-
-        cpus = psutil.cpu_percent(interval=None, percpu=True)
-
-        # 获取系统cpu使用率 non-blocking
-
-        socketio.emit('server_response',
-
-                      {'data': [t, cpus], 'count': count},
-
-                      namespace='/test')
-
-        socketio.emit('messageEventNew',
-
-                      {'encryptkey': 'key'},
-
-                      namespace='/test')
-
-        # 注意：这里不需要客户端连接的上下文，默认 broadcast = True
-
 if __name__ == '__main__':
     # compatible to both polling and websocket
     #TODO: create learning modle
-
-
 
     eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 5000)), app, )
