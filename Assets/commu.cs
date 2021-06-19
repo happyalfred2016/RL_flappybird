e83c44acb274bfdd7188b1e54dfd76e4927d9ceb
Alfred <gdzzc96@gmail.com>
2021-06-19 22:26:58 +0800

    1. update framework 2. implemented screenshot capture 3. implemented image decoding in server

--- Assets/commu.cs
+++ Assets/commu.cs
@@ -11,8 +11,9 @@ public class commu : MonoBehaviour
 {
     private SocketManager manager = new SocketManager(new Uri("http://127.0.0.1:5000/socket.io/"));
     private int count = 0;
-    private int perframe = 9999999;
+    private int perframe = 60;
     private bool actioned = false;
+    private bool activted = false;
     void Start ()
     {
         manager.Open();
@@ -25,10 +26,32 @@ public class commu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((count % perframe == 0) | actioned)
+        // 
+        // if (activted)
+        // {
+        //     if ((count % perframe == 0) & activted)
+        //     {
+        //         while(true)
+        //             if (actioned)
+        //             {
+        //                 var bytes = capture();
+        //                 manager.Socket.Emit("chat", "User", "Sending Picture");
+        //                 manager.Socket.Emit("obs", bytes);
+        //                 actioned = false;
+        //                 // count = 0;
+        //                 break;
+        //             }
+        //     }
+        // }
+        // else if (actioned)
+        //     activted = true;
+        
+        // TODO: wait until action reach.(In order to action with fixed time interval)
+        if (actioned)
         {
+            var bytes = capture();
             manager.Socket.Emit("chat", "User", "Sending Picture");
-            manager.Socket.Emit("picture", capture());
+            manager.Socket.Emit("obs", bytes);
             actioned = false;
             count = 0;
         }
@@ -47,8 +70,34 @@ public class commu : MonoBehaviour
         actioned = true;
     }
 
-    byte capture()
+    byte[] capture()
     {
-        return new byte();
+        int resWidth = 3300; 
+        int resHeight = 2550;
+        return CaptureScreenshot2(new Rect(0, 0, resWidth, resHeight));
     }
+    
+    byte[] CaptureScreenshot2(Rect rect)
+    {
+        var camera = Camera.main;
+        RenderTexture rt = new RenderTexture((int)rect.width, (int)rect.height, 24);
+        camera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.ARGB32, false);
+        camera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(rect, 0, 0);
+        camera.targetTexture = null;
+        RenderTexture.active = null; 
+        Destroy(rt);
+        byte[] bytes = screenShot.EncodeToPNG();
+        // string filename = Application.dataPath + "/Screenshot.png";  
+        //
+        // System.IO.File.WriteAllBytes(filename, bytes);
+        // Debug.Log(string.Format("Took screenshot to: {0}", filename));
+        // // Application.OpenURL(filename);
+        return bytes;
+    }
+    
+    
+    
 }
--- commu_server.py
+++ commu_server.py
@@ -1,29 +1,43 @@
 import eventlet
 import socketio
+import io
+import PIL
+import numpy as np
+import logging
 import time
+
+logging.basicConfig(level='INFO')
 
 sio = socketio.Server(binary=True)
 # app = socketio.WSGIApp(sio, static_files={
 #     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 # })
 app = socketio.WSGIApp(sio)
+# RL model
+model = None
+
+
+# TODO: fix the case that too many obs that algorithm can not handle.
 
 @sio.on('connect')
 def connect(sid, environ):
+    sio.emit('action', 0)
     print('connect ', sid)
+
 
 @sio.on('chat')
 def chat(sid, user, meg):
     print('chat: ', user, meg)
 
-@sio.on('picture')
-def chat(sid, pic_byte):
-    # TODO: learning process
-    time.sleep(1)
-    print('chat: ', pic_byte)
 
-    # TODO: send action to client
-    sio.emit('action', 'click')
+@sio.on('obs')
+def obs(sid, pic_byte):
+    # rlflappybird.add_obs(pic_byte, status={})
+    sio.start_background_task(rl_model, pic_byte, {})
+
+    print('received bytes')
+    eventlet.sleep(2)
+    sio.emit('action', 0)
 
 
 @sio.on('disconnect')
@@ -31,8 +45,30 @@ def disconnect(sid):
     print('disconnect ', sid)
 
 
-if __name__ == '__main__':
-    # compatible to both polling and websocket
-    #TODO: create learning modle
+def byte2img(bytes):
+    # no idea why have a extra byte at the beginning
+    picture_stream = io.BytesIO(bytes[1:])
+    picture = PIL.Image.open(picture_stream)
+    imMat = np.asarray(picture)[:, :, :3]
 
+    # TODO: resize the image
+    return imMat
+
+
+def rl_model(bytes, status):
+    img = byte2img(bytes)
+    status = status
+    logging.info('Obs Reveived')
+
+    # TODO: learning process
+    # model.fit
+    action = 0
+    time.sleep(3)
+
+    # self.sio.emit('action', action)
+    sio.emit('action', 0)
+    logging.info('Action Sent')
+
+
+if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 5000)), app, )
